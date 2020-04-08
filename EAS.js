942facd9197156d5c124531aba65095b8692e1c5
Bret Rivera <bdrrivera@gmail.com>
2020-04-08 11:39:31 -0700

    Added method that establishes basic CSS that will be used by the EtchAsketch, also put place holder elements in stylesheet and changed name of parent div for EtchASketch

--- EAS.js
+++ EAS.js
@@ -1,3 +1,30 @@
 'use strict';
 
+/* Global Variable Block */
+var boxSquareCount = 16;
+var etchDivs;
+
+/* Runtime Start*/
+
+
+/* Runtime End */
+
+
+/**
+ * Establishes basic CSS 
+ * 
+ */
+function setUpCSS() {
+    const etchCont = $('#etchContainer');
+    etchCont.style.display = "grid";
+    etchCont.style.width = "900";
+}
+
+function drawBoxes() {
+    
+    for(var i = 0; i < boxSquareCount; i++) {
+
+    }
+}
+
 function $(n) { return document.querySelector(n); }
