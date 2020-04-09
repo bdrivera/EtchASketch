2b6f1fba4412047cbfd3c5e92fb7c4647f6d869a
Bret Rivera <bdrrivera@gmail.com>
2020-04-08 17:58:23 -0700

    Added buttons and button eventListener
    -Added buttons via HTML
    -Added functioning eventListeners, which refer to buttonEvents function
    to better organize functionality

--- EAS.js
+++ EAS.js
@@ -1,23 +1,32 @@
 'use strict';
 
 /* Global Variable Block */
-const etchCont = $('#etchContainer');//constant for board div
-const etchButt = $('#etchButtonContainer');//constant for buttons div
-var boxSquareCount = 16;
-var etchContWidth = 900;
-var etchDivs = new Array();
-var activeEffectId = 0;
+const etchCont = $('#etchContainer'); //constant for board div
+const etchButt = $('#etchButtonContainer'); //constant for buttons div
+var boxSquareCount = 16; //length of one side of the board in blocks
+var etchContWidth = 900; //width in pixels of the board container
+var etchDivs = new Array(); //array holding board blocks objects
+
+/*
+ * The effect to be applied when hovering over a block.
+ *   0=Black
+ *   1=White
+ *   2=Gradient
+ *   3=Random
+ */
+var activeEffectId = 1;
 
 /* Runtime Start*/
 setUpCSS();
 drawBoxes();
+addButtonListeners();
 addBoardListeners();
 
 /* Runtime End */
 
 
 /**
- * Establishes basic CSS
+ * Establishes basic CSS for Etch-A-Sketch
  */
 function setUpCSS() {
     //CSS for board
@@ -30,20 +39,13 @@ function setUpCSS() {
 }
 
 /**
- * Draws buttons to manipulate board functions
- */
-function drawButtons() {
-
-}
-
-/**
  * Draws div boxes on the board
  */
 function drawBoxes() {
 
     for(var i = 0; i < (boxSquareCount * boxSquareCount); i++) {
         etchDivs[i] = document.createElement('div');
-        etchDivs[i].classList.add('content');
+        etchDivs[i].classList.add('etchBox' + i);
         etchDivs[i].textContent = i;
         etchDivs[i].style.backgroundColor = "black";
         etchDivs[i].style.color = "white";
@@ -55,25 +57,74 @@ function drawBoxes() {
 }
 
 /**
+ * Adds event listeners to all boxes used to control the
+ * etch-a-sketch board
+ */
+function addButtonListeners() {
+    const butts = document.querySelectorAll('button');
+    butts.forEach((button) => {
+        button.addEventListener('click',
+            function(e) {
+                buttonEvent(e.target);
+            });
+    });
+}
+
+/**
  * Adds event listeners to all div boxes in the etch-a-sketch board
  */
 function addBoardListeners() {
     for(var i = 0; i < (boxSquareCount * boxSquareCount); i++) {
         etchDivs[i].addEventListener('mouseenter',
-            function(e){
-                eventEffect(e.target);
+            function(e) {
+                boardEvent(e.target);
             });
     }
 }
 
 /**
- * Uses provided paramater to execute an event
- * @param {*} d 
+ * Uses provided parameter to execute button event
+ * @param {*} b target button
  */
-function eventEffect(d) {
+function buttonEvent(b) {
+    switch(b.value) {
+        case 0: //Clear
+
+        return;
+        case 1: //Draw
+
+        return;
+        case 2: //Gradient
+
+        return;
+        case 3: //Random
+
+        return;
+        case 4: //Erase
+
+        return;
+    }
+}
+
+/**
+ * Uses provided paramater to execute an event
+ * @param {*} d target div
+ */
+function boardEvent(d) {//TODO
     console.log("triggered: " + d.textContent);
-    if(activeEffectId == 0) {
-        d.style.backgroundColor = "red";
+    switch(activeEffectId) {
+        case 0: //Black
+            d.style.backgroundColor = "black";
+        return;
+        case 1: //White
+            d.style.backgroundColor = "white";
+        return;
+        case 2: //Gradient
+
+        return;
+        case 3: //Random
+
+        return;
     }
 }
 
